c71181fa6ffbf28811e917e721a3b311e4ad0ea9
kgliwinski <259282@student.pwr.edu.pl>
2023-02-14 19:54:03 +0100

    Resolved build issues

--- Mainboard_ESP32/components/cli/console_commands.c
+++ Mainboard_ESP32/components/cli/console_commands.c
@@ -2,19 +2,10 @@
 
 #include <stdlib.h>
 #include "console_commands.h"
-#include "state_machine.h"
-#include "esp_log.h"
-#include "esp_system.h"
-#include "brake_servo.h"
-#include "recovery_servo.h"
-#include "flash_nvs.h"
-#include "rtos_tasks.h"
-#include "nvs.h"
+
 
 #define TAG "CLI"
 
 int CLI_echo(int argc, char **argv) {
-    uint8_t state = SM_get_current_state();
-    ESP_LOGI(TAG, "Current state %d", state);
     return 0;
 }
